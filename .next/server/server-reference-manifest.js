ff4e30424a915116b907d596d622d57934c312fa
annxpx <apalvaradol@outlook.com>
2024-03-17 18:55:31 -0600

    initialization

---
+++ .next/server/server-reference-manifest.js
@@ -0,0 +1,1 @@
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"32wBGUCMMjApTbcGqn7gUXjh7gPAg6Kol65rdkJ6KoE=\"\n}"
---
+++ app/auth/login/page.tsx
@@ -0,0 +1,61 @@
+import { Button } from '@/components/ui/button'
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from '@/components/ui/card'
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from '@/components/ui/form'
+import { Input } from '@/components/ui/input'
+import React from 'react'
+import { useForm } from 'react-hook-form'
+
+const LoginPage = () => {
+  const form = useForm()
+
+  return (
+      <Card className='w-[400px]'>
+        <CardHeader>
+          <CardTitle>
+            ¡Bienvenido! 
+          </CardTitle>
+          <CardDescription>
+            Inicie sesión con su cuenta
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <Form {...form}>
+            <FormField 
+            control={form.control}
+            name='email'
+            render={({field})=> (
+              <FormItem>
+                <FormLabel>Correo Electronico</FormLabel>
+                <FormControl>
+                  <Input placeholder='example@mail.com' {...field}/>
+                </FormControl>
+                <FormMessage/>
+              </FormItem>
+
+            )}>
+            </FormField>
+            <FormField 
+            control={form.control}
+            name='password'
+            render={({field})=> (
+              <FormItem>
+                <FormLabel>Contraseña</FormLabel>
+                <FormControl>
+                  <Input type='password' {...field}/>
+                </FormControl>
+                <FormMessage/>
+              </FormItem>
+            )}>
+            </FormField>
+          </Form>
+        </CardContent>
+        <CardFooter className='flex items-end justify-end'>
+          <Button>
+            Continuar
+          </Button>
+        </CardFooter>
+      </Card>
+  )
+}
+
+export default LoginPage
