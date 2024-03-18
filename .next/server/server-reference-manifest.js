e4b2e6a3352640bdedb57f1e2a5b542906139170
annxpx <apalvaradol@outlook.com>
2024-03-17 19:14:49 -0600

    Resolve conflicts

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"32wBGUCMMjApTbcGqn7gUXjh7gPAg6Kol65rdkJ6KoE=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"0gSMQG6d3VWGClytKT8lRS46m5istdIq2urdEemTKM4=\"\n}"
